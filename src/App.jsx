afc599eff704a777476d32cb4d57d78731c2ac94
aykaimran <aykaimran28@gmail.com>
2026-03-11 11:51:11 +0500

    Few Changes Made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Login from './pages/Login/Login'
